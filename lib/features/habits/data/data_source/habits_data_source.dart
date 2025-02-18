1bfe84ca639fac91c0b4e5a3138fa8fe160acfa8
roman-liutyk <lytikroman5@gmail.com>
2025-02-18 12:02:26 +0100

    feat: adds habits data source

---
+++ lib/features/habits/data/data_source/habits_data_source.dart
@@ -0,0 +1,79 @@
+import 'package:drift/drift.dart';
+import 'package:habit_tracker/database/database.dart';
+
+abstract class HabitsDataSource {
+  Future<List<HabitsTableData>> fetchHabits({
+    required int groupId,
+  });
+
+  Future<int> addHabit({
+    required int groupId,
+    required String title,
+  });
+
+  Future<void> updateHabit({
+    required int habitId,
+    required bool completed,
+  });
+
+  Future<void> deleteHabit({
+    required int habitId,
+  });
+}
+
+class HabitsDataSourceImpl implements HabitsDataSource {
+  const HabitsDataSourceImpl({
+    required HabitsDatabase db,
+  }) : _db = db;
+
+  final HabitsDatabase _db;
+
+  @override
+  Future<int> addHabit({
+    required int groupId,
+    required String title,
+  }) async {
+    return await _db.into(_db.habitsTable).insert(
+          HabitsTableCompanion.insert(
+            title: title,
+            group: groupId,
+            completed: false,
+            createdAt: DateTime.now(),
+            lastModifiedAt: DateTime.now(),
+          ),
+        );
+  }
+
+  @override
+  Future<void> deleteHabit({
+    required int habitId,
+  }) async {
+    await _db.habitsTable.deleteWhere((data) => data.id.equals(habitId));
+  }
+
+  @override
+  Future<List<HabitsTableData>> fetchHabits({
+    required int groupId,
+  }) async {
+    return await (_db.select(_db.habitsTable)
+          ..where((data) => data.id.equals(groupId)))
+        .get();
+  }
+
+  @override
+  Future<void> updateHabit({
+    required int habitId,
+    required bool completed,
+  }) async {
+    await (_db.update(_db.habitsTable)
+          ..where((data) => data.id.equals(habitId)))
+        .write(
+      HabitsTableCompanion(
+        completed: Value(completed),
+        lastModifiedAt: Value(
+          DateTime.now(),
+        ),
+      ),
+    );
+  }
+}
